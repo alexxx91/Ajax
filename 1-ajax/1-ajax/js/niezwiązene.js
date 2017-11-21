6438b3cec83da6b3fa745aab175856e58dcaad7c
Aleksandar Korach <aleksandar.korach@gmail.com>
2017-11-21 21:10:58 +0100

    dane-programisty

---
+++ 1-ajax/1-ajax/js/niezwiązene.js
@@ -0,0 +1,11 @@
+function dodawanie() {
+    var a = 2;
+    var b = 3;
+    
+    var wynik = a + b;
+    //console.log(wynik);
+    return wynik;
+}
+
+var zwracanaWartosc = dodawanie();
+alert(zwracanaWartosc);
